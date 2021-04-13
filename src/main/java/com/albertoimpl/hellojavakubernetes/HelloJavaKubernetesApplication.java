b5e9d36703a1c664737c882443f0e61d629fc920
Alberto C. Ríos <albertoimpl@gmail.com>
2021-04-13 15:59:30 +0200

    Initial commit

---
+++ src/main/java/com/albertoimpl/hellojavakubernetes/HelloJavaKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.albertoimpl.hellojavakubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJavaKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/albertoimpl/hellojavakubernetes/HelloJavaKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertoimpl.hellojavakubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
